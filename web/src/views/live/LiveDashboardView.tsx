58eaee74d6de518a848f9d4180b33841ca2d0001
Josh Hawkins <32435876+hawkeye217@users.noreply.github.com>
2025-02-17 08:19:03 -0600

    Improve notifications (#16632)
    * add notification cooldown
    
    * cooldown docs
    
    * show alert box when notifications are used in an insecure context
    
    * add ability to suspend notifications from dashboard context menu

--- web/src/views/live/LiveDashboardView.tsx
+++ web/src/views/live/LiveDashboardView.tsx
@@ -510,6 +510,7 @@ export default function LiveDashboardView({
                   resetPreferredLiveMode={() =>
                     resetPreferredLiveMode(camera.name)
                   }
+                  config={config}
                 >
                   <LivePlayer
                     cameraRef={cameraRef}
