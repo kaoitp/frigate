ccc48c40c0681535a320666358b389183cf8a399
Nicolas Mowen <nickmowen213@gmail.com>
2025-03-03 16:28:34 -0700

    Quick fix (#16926)
    * fix
    
    * Fix
    
    * Fix incorrect default websocket value
    
    * Cleanup value setting

--- web/src/components/player/LivePlayer.tsx
+++ web/src/components/player/LivePlayer.tsx
@@ -204,6 +204,9 @@ export default function LivePlayer({
   const prevCameraEnabledRef = useRef(cameraEnabled ?? true);
 
   useEffect(() => {
+    if (cameraEnabled == undefined) {
+      return;
+    }
     if (!prevCameraEnabledRef.current && cameraEnabled) {
       // Camera enabled
       setLiveReady(false);
