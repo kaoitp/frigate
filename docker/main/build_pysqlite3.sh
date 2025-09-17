98efb5eb6efea6d1a94fa5dbc5ac3bcd86a5e845
kaoitp <itpanw@gmail.com>
2025-09-17 15:20:05 +0700

    update sqlite to 3.46.1

--- docker/main/build_pysqlite3.sh
+++ docker/main/build_pysqlite3.sh
@@ -2,18 +2,17 @@
 
 set -euxo pipefail
 
-SQLITE3_VERSION="3.46.0"
+SQLITE3_VERSION="3.46.1"
 PYSQLITE3_VERSION="0.5.3"
 
-# Fetch the pre-built sqlite amalgamation instead of building from source
 # Fetch the pre-built sqlite amalgamation instead of building from source
 if [[ ! -d "sqlite" ]]; then
   mkdir sqlite
   cd sqlite
   
   # Download the pre-built amalgamation from sqlite.org
-  # For SQLite 3.46.0, the amalgamation version is 3460000
-  SQLITE_AMALGAMATION_VERSION="3460000"
+  # For SQLite 3.46.1, the amalgamation version is 3460100
+  SQLITE_AMALGAMATION_VERSION="3460100"
   
   wget https://www.sqlite.org/2024/sqlite-amalgamation-${SQLITE_AMALGAMATION_VERSION}.zip -O sqlite-amalgamation.zip
   unzip sqlite-amalgamation.zip
